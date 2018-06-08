eb8595f1292c2b8014957abb5785a73cabb8f764
刘悦 <liuyue@wanfangdata.com.cn>
2018-06-08 15:22:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li v-for="(item,key) in data">{{item}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      data:[]
+    }
+  },
+  mounted:function(){
+    this.$http.get('/api/data').then(res=>{
+      this.data=res.data;
+      console.log(res)
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
